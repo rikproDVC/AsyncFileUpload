84f3d1719974416967a28a68f8eba7d29c23a819
Rik pronk <rikpronkie@gmail.com>
2015-09-22 10:07:58 +0200

    Enabled multiple file selection, clarified + commented code a bit more, added image preview functionality

--- src/AspNet.AzureStorage.Sample/Scripts/fileupload.js
+++ src/AspNet.AzureStorage.Sample/Scripts/fileupload.js
@@ -2,57 +2,25 @@
 var fileObj = new Object;
 var listCount = 0;
 
-var deleteFileFromList = function(element) {
-    var fileId = $(element).parent('li').attr('id');
-    $("#" + fileId).remove();
-    delete fileObj[fileId];
-}
-
-var compareFiles = function(file1, file2) {
-    return file1.size == file2.size && file1.lastModified == file2.lastModified;
-}
-
 $('#uploadfield').on('change', function (e) {
     var files = e.target.files;
     if (files.length > 0) {
-        if (window.FormData !== undefined) {
-            for (var x = 0; x < files.length; x++) {
-                var counter = 0;
-                for (var key in fileObj) {
-                    if (fileObj.hasOwnProperty(key) && compareFiles(fileObj[key], files[x])) {
-                        counter++;
-                    }
-                }
-                if (counter < 1) {
-                    var fileKey = "file-" + listCount;
-                    listCount++;
-
-                    // Add the file to object as property
-                    fileObj[fileKey] = files[x];
-
-                    $('<li></li>', {
-                        id: fileKey
-                    }).html(files[x].name + '<button class="remove" onclick="deleteFileFromList(this)">X</button>')
-                        .appendTo('#Filelist');
-                }    
-            }
-            $(e.target).parent('form').trigger('reset');
-        } else {
-            alert('This browser doesn\'t support HTML5 file uploads!');
-        }
+        processFileSelection(e, files)
     }
 });
 
-
 $('#uploadsubmit').on('click', function (e) {
+    // Prevent sending the form
     event.preventDefault();
 
+    // Add all non-standard properties of the file object to the form data
     for (var key in fileObj) {
         if (fileObj.hasOwnProperty(key)) {
             data.append(fileObj[key].name, fileObj[key]);
         }
     }
 
+    // Send the ajax call
     $.ajax({
         type: "POST",
         url: '/File/UploadAjax',
@@ -61,8 +29,9 @@ $('#uploadsubmit').on('click', function (e) {
         data: data,
         success: function (result) {
             console.log(result);
+            // Clear the file object and file list
             fileObj = new Object;
-            $('#Filelist').html("");
+            $('table').html("");
         },
         error: function (xhr, status, p3, p4) {
             var err = "Error " + " " + status + " " + p3 + " " + p4;
@@ -72,3 +41,52 @@ $('#uploadsubmit').on('click', function (e) {
         }
     });
 });
+
+var deleteFileFromList = function(element) {
+    var fileId = $(element).parents('tr').attr('id');
+    $("#" + fileId).remove();
+    delete fileObj[fileId];
+}
+
+var compareFilesForDuplicate = function(file1, file2) {
+    return file1.size == file2.size && file1.lastModified == file2.lastModified;
+}
+
+var processFileSelection = function (event, files) {
+    if (window.FormData !== undefined) {
+        for (var x = 0; x < files.length; x++) {
+            var counter = 0;
+            // Compare files for duplicates
+            for (var key in fileObj) {
+                if (fileObj.hasOwnProperty(key) && compareFilesForDuplicate(fileObj[key], files[x])) {
+                    counter++;
+                }
+            }
+
+            // Only process the files when no duplicates are found
+            if (counter < 1) {
+                var fileKey = "file-" + listCount;
+                listCount++;
+
+                // Add the file to object as property
+                fileObj[fileKey] = files[x];
+
+                // Generate new table row for file
+                var row = $('<tr></tr>', {
+                    id: fileKey
+                    });
+
+                $('<td></td>').html('<img class="imageUploadPreview" src="' + URL.createObjectURL(files[x]) + '">').appendTo(row);
+                $('<td></td>').html(files[x].name).appendTo(row);
+                $('<td></td>').html('<button class="remove" onclick="deleteFileFromList(this)">X</button>').appendTo(row);
+
+                row.appendTo('table');
+            }
+        }
+        // Reset the form field so the upload button in empty again
+        $(event.target).parent('form').trigger('reset');
+    } else {
+        alert('This browser doesn\'t support HTML5 file uploads!');
+    }
+}
+
